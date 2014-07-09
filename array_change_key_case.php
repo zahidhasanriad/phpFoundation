feb491237ab38e388ecd746533cec980bd74a29e
zahid <zahid.riadd@gmail.com>
2014-07-09 22:10:53 +0600

    Addition of upper to lower case letter

--- array_change_key_case.php
+++ array_change_key_case.php
@@ -3,3 +3,10 @@
 $input_array = array("FirSt" => 1, "SecOnd" => 2); 
 
 print_r(array_change_key_case($input_array, CASE_UPPER)); // All lower case letter will be upper case
+
+echo "<br />";
+
+$input_array = array("FirSt" => 1, "SecOnd" => 2); 
+
+print_r(array_change_key_case($input_array, CASE_LOWER)); // All upper case letter will be lower case
+

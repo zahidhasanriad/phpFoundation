86d9f405236f07de6e036bcbe77831d5e11a71ed
zahid <zahid.riad@gmail.com>
2014-07-09 14:28:28 +0600

    only using marks>79 to get A+

--- ifelse.php
+++ ifelse.php
@@ -1,27 +1,27 @@
 <?php
 
-//$marks=73;
+$marks=73;
 //$marks=63;
-$marks=43;
+//$marks=43;
 
-if($marks >= 80 && $marks <= 100)
+if($marks > 79)
 	$grade="A+";
-else if($marks >= 70 && $marks <= 79)
+else if($marks >69 )
 	$grade="A";
 
-else if($marks >= 60 && $marks <= 69)
+else if($marks > 59 )
 	$grade="A-";
 
-else if($marks >= 50 && $marks <= 59)
+else if($marks > 49 )
 	$grade="B";
 
-else if($marks >= 40 && $marks <= 49)
+else if($marks >39)
 	$grade="C";
 
-else if($marks >= 33 && $marks <= 39)
+else if($marks >32)
 	$grade="D";
 
-else if($marks >= 0 && $marks <= 32)
+else if($marks <32)
 	$grade="F";
 
 echo $grade;

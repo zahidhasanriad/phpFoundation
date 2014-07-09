adb776255a5e385298b43c45e2fc9eaed4fb4ee2
zahid <zahid.riad@gmail.com>
2014-07-09 13:13:06 +0600

    array declaration techniques

--- array.php
+++ array.php
@@ -7,3 +7,34 @@ $cars = array(); //variable is defined as an array
 echo gettype($cars); //array output
 
 echo '<br/>........................<br/>' ;
+
+$cars = array('toyota','sedan','bmw'); //variable is defined as an array
+
+echo "pre";
+print_r($cars);
+echo "</pre">;
+echo '<br/>........................<br/>' ;
+
+$cars[]='premeo';
+
+echo "pre";
+print_r($cars);
+echo "</pre">;
+echo '<br/>........................<br/>' ;
+
+
+$cars[8]='suzuki';
+
+echo "pre";
+print_r($cars);
+echo "</pre">;
+echo '<br/>........................<br/>' ;
+
+$cars[]='corola';
+
+echo "pre";
+print_r($cars);
+echo "</pre">;
+echo '<br/>........................<br/>' ;
+
+

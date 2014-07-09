d61641422229bcfa914ed320a8693e0616de95b1
zahid <zahid.riad@gmail.com>
2014-07-09 11:40:09 +0600

    inital commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php 
+//echo "Hollo FTFL LARAVEL";
+
+//Syntax Exercise
+
+//variables
+
+//$a = 1;
+
+//block
+//nested
+
+{
+	{
+
+	}
+}
+
+//Data Type
+
+//$aNumber = 1234 ;
+
+//print variable type
+//echo gettype($aNumber);
+
+//echo settype($aNumber, 'String');
+
+//echo gettype($aNumber);
+
+//Difference between single quote & double quote
+
+//$aString = abcd;
+//$aString ='abcd$aNumber';
+//$anotherString = "abcd$aNumber";
+
+//echo gettype($aString);
+//echo gettype($anotherString);
+
+//echo $aString;
+//echo "<br />";
+//echo $anotherString;
+
+//$aFloatNumber = 12.123;
+//echo gettype($aFloatNumber);
+
+/*$aBool = true;
+$anotherBool = false ;
+
+echo gettype($aBool) ;
+echo gettype($anotherBool) ;
+
+echo $aBool;
+Echo $anotherBool; */
+
+//operator precedence
+
+//$a = 2*(3+5);
+//echo $a;
+
+//$a = 2*3 + 5;
+//echo $a."<br/>";
+//echo $a;
+
+//echo nl2br($a."\n".$a);
+
+$name = 'zahid'; //what is my n
+for($i=1; $i<=5; $i++){
+
+
+
+
+}
+
+
+//data type
+
+//stongly typed c# & php weekly typed data type, static & dynamic

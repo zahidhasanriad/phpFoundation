a145cf827b54493ea0dea67a0088dec977b0dc34
zahid <zahid.riad@gmail.com>
2014-07-09 13:28:06 +0600

    multidimensional

--- array.php
+++ array.php
@@ -6,21 +6,21 @@ $cars = array(); //variable is defined as an array
 
 echo gettype($cars); //array output
 
-echo '<br/>........................<br/>' ;
+echo "<br/>........................<br/>" ;
 
 $cars = array('toyota','sedan','bmw'); //variable is defined as an array
 
 echo "pre";
 print_r($cars);
 echo "</pre>";
-echo '<br/>........................<br/>' ;
+echo "<br/>........................<br/>" ;
 
 $cars[]='premeo';
 
 echo "pre";
 print_r($cars);
 echo "</pre>";
-echo '<br/>........................<br/>' ;
+echo "<br/>........................<br/>" ;
 
 
 $cars[8]='suzuki';
@@ -28,14 +28,14 @@ $cars[8]='suzuki';
 echo "pre";
 print_r($cars);
 echo "</pre>";
-echo '<br/>........................<br/>' ;
+echo "<br/>........................<br/>" ;
 
 $cars[]='corola';
 
 echo "pre";
 print_r($cars);
 echo "</pre>";
-echo '<br/>........................<br/>' ;
+echo "<br/>........................<br/>" ;
 
 //associative array
 
@@ -45,6 +45,14 @@ $fruits[] = 'this is a value';
 echo "pre";
 print_r($fruits);
 echo "</pre>";
-echo $fruits['a'];
-echo '<br/>........................<br/>' ;
+//echo $fruits['a'];
+echo "<br/>........................<br/>" ;
 
+//multidimensional array
+
+$arr2D = array($cars,$fruits);
+echo "pre";
+print_r($arr2D);
+echo "</pre>";
+
+echo "<br/>........................<br/>" ;

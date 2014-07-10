b9556f74087a11e72dcc0d71850344cd32a73e0c
zahid <zahid.riad@gmail.com>
2014-07-10 11:48:42 +0600

    with slug

--- films.php
+++ films.php
@@ -1,8 +1,6 @@
 <?php
 
 
-
-
  function getInfoFromGenres($genre)
     {   
         $films = array(
@@ -18,28 +16,28 @@
         if($genre == $films['genres'][0])
         {
             echo 'Film Title : '.$films['film_titles'][0].' ('.  strtolower($films['film_titles'][0]).')<br/>';
-            echo 'Stars : '.$films['stars'][0].' ('.  strtolower($films['stars'][0]).')<br/>';
+            echo 'Stars : '.$films['stars'][0].' ('.  strtolower($films['stars'][0]).')<br/>'.str_replace(" ","-", strtolower($films['stars'][0]));
             
         }
         
         else if($genre == $films['genres'][1])
         {
             echo 'Film Titles : '.$films['film_titles'][1].' ('.  strtolower($films['film_titles'][1]).')<br/>';
-            echo 'Stars : '.$films['stars'][1].' ('.  strtolower($film['stars'][1]).')<br/>';
+            echo 'Stars : '.$films['stars'][1].' ('.  strtolower($film['stars'][1]).')<br/>'.str_replace(" ","-", strtolower($films['stars'][0]));
             
         }
         
         else if($genre == $film['genres'][2])
         {
             echo 'Film Title : '.$films['film_titles'][2].' ('.  strtolower($films['film_titles'][2]).')<br/>';
-            echo 'Stars : '.$films['stars'][2].' ('.  strtolower($films['stars'][2]).')<br/>';
+            echo 'Stars : '.$films['stars'][2].' ('.  strtolower($films['stars'][2]).')<br/>'.str_replace(" ","-", strtolower($films['stars'][0]));
             
         }
         
         else if($genre == $films['genres'][3])
         {
             echo 'Film Title : '.$films['film_titles'][3].' ('.  strtolower($films['film_title'][3]).')<br/>';
-            echo 'Stars : '.$films['stars'][3].' ('.  strtolower($films['stars'][3]).')<br/>';
+            echo 'Stars : '.$films['stars'][3].' ('.  strtolower($films['stars'][3]).')<br/>'.str_replace(" ","-", strtolower($films['stars'][0]));
             
         }
         

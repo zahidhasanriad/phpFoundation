1a7ec777100a4cb88617d5cec27a5a713fdc2ff1
zahid <zahid.riad@gmail.com>
2014-07-10 11:44:38 +0600

    2nd edit

--- films.php
+++ films.php
@@ -1,6 +1,11 @@
 <?php
 
-$films = array(
+
+
+
+ function getInfoFromGenres($genre)
+    {   
+        $films = array(
 
 				'genres' => array("comedy","tragedy","action","romance"),				
 				
@@ -10,21 +15,37 @@ $films = array(
 
 				);
 
+        if($genre == $films['genres'][0])
+        {
+            echo 'Film Title : '.$films['film_titles'][0].' ('.  strtolower($films['film_titles'][0]).')<br/>';
+            echo 'Stars : '.$films['stars'][0].' ('.  strtolower($films['stars'][0]).')<br/>';
+            
+        }
+        
+        else if($genre == $films['genres'][1])
+        {
+            echo 'Film Titles : '.$films['film_titles'][1].' ('.  strtolower($films['film_titles'][1]).')<br/>';
+            echo 'Stars : '.$films['stars'][1].' ('.  strtolower($film['stars'][1]).')<br/>';
+            
+        }
+        
+        else if($genre == $film['genres'][2])
+        {
+            echo 'Film Title : '.$films['film_titles'][2].' ('.  strtolower($films['film_titles'][2]).')<br/>';
+            echo 'Stars : '.$films['stars'][2].' ('.  strtolower($films['stars'][2]).')<br/>';
+            
+        }
+        
+        else if($genre == $films['genres'][3])
+        {
+            echo 'Film Title : '.$films['film_titles'][3].' ('.  strtolower($films['film_title'][3]).')<br/>';
+            echo 'Stars : '.$films['stars'][3].' ('.  strtolower($films['stars'][3]).')<br/>';
+            
+        }
+        
+}
 
-	/*function getInfoFromGenres($films['genres'])
-		{
-
-			
-			
-
-		}	*/
-
-		foreach ($films as $key => $value){
-			
-			if ($key === 'genres' && $value === 'comedy'){
-
-				print_r ($films);
-			}
+getInfoFromGenres('comedy');
 
 
 			

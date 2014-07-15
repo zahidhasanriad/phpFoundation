0d36a847eca33c6a24108bd4e035cd184bab011e
zahidhasanriad <zahid.riad@gmail.com>
2014-07-15 10:39:53 +0600

    AssociationRelationOfCustomerAccount

---
+++ AssociationRealtionOfCustomerAccount/account.php
@@ -0,0 +1,57 @@
+<?php
+
+
+class Account {
+    
+    private $account_number;
+    private $opening_date;
+    private $balance;
+    
+    
+    function __construct($account_number, $opening_date) {
+        $this->account_number = $account_number;
+        $this->opening_date = $opening_date;
+        $this->balance = 0;
+    }
+    
+    public function get_account_number() {
+        return $this->account_number;
+    }
+
+    public function get_opening_date() {
+        return $this->opening_date;
+    }
+
+    public function get_balance() {
+        return $this->balance;
+    }
+
+   
+    
+    
+    public function deposit($amount)
+           {
+        
+        $this->balance += $amount;
+        
+            return 'deposited';
+           }
+           
+           public function withdraw($amount)
+           {
+        
+              if($this->balance - $amount >=0){
+                    $this->balance -= $amount;
+                return 'withdrwan';
+              }
+              
+                else { 
+                    return 'insufficient balance';
+                    
+                }
+            }
+
+
+
+    
+}
---
+++ AssociationRealtionOfCustomerAccount/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+         <style>
+            
+            body 
+                {
+                    background-color: #d0e4fe;
+                    font-family: "Times New Roman";
+                    font-size: 20px;
+                    color: #AFB798;
+                }
+
+            
+
+            
+
+            input
+                {
+                    color: #A98DF8;
+                }
+                
+        </style>
+        
+    </head>
+   
+    <body>
+    
+            
+        <form action="index.php"  method="GET">
+           
+            Customer Account Info Entry
+            Customer Name: <input type="text" name='customerNameText'> <br />
+             Email Address: <input type="text" name='emailAddressText'> <br/>
+            Account Number: <input type="text" name='accountNumberText'> <br/>
+            Opening Date: <input type="text" name='openingDateText'> <br/>
+             
+             <input type='submit' name='createButton' value='Create Account'/> <br />
+             Transaction <br/>
+            Amount: <input type="text" name='amountText'> <br />
+            <input type='submit' name='depositButton' value='Deposit'/>
+            <input type='submit' name='withdrawButton' value='Withdraw'/><br />
+            <input type='submit' name='showReportButton' value='Show Report'/>
+           </form>
+         
+        <?php
+        
+        require 'account.php';
+        require 'customer.php';
+        
+        session_start();
+        
+         if(isset($_GET["createButton"]))
+            {
+       
+        $customer = new Customer();
+        
+        $customer->set_customer_name($_GET['customerNameText']);
+        $customer->set_email_address($_GET['emailAddressText']);
+        
+         $account = new Account($_GET['accountNumberText'], $_GET['openingDateText']);
+        $customer->set_customer_account($account);
+        
+        $_SESSION['a_customer'] = $customer;
+        
+            
+           echo 'created';
+          }
+        
+         if(isset($_GET['depositButton']))
+            {
+                       $customer = $_SESSION['a_customer'];
+                       
+                       $customer->get_customer_account()->deposit($_GET['amountText']);
+                       
+               
+                       $_SESSION['a_customer'] = $customer;
+                       
+                       echo 'deposited';
+            }
+           
+             if(isset($_GET['withdrawButton']))
+            {
+                   $customer = $_SESSION['a_customer'];
+                       
+                       echo $customer->get_customer_account()->withdraw($_GET['amountText']);
+                       
+                       $_SESSION['a_customer'] = $customer;
+                       
+            }
+            
+            if(isset($_GET['showReportButton']))
+            {
+              $customer = $_SESSION['a_customer'];
+                 
+              echo $customer->get_customer_account()->get_account_number().'<br/>';
+              echo $customer->get_customer_name().'<br/>';
+              echo $customer->get_customer_account()->get_opening_date().'<br/>';
+              echo $customer->get_customer_account()->get_balance().'<br/>';
+              
+                  
+             }
+        
+       
+        
+        
+        
+        
+            
+        
+        
+        ?>
+    </body>
+    
+</html>
